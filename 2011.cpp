b67e6f407afcf86ff7ab96bd13e7d96e3082d06b
OracleMahad <61798131+OracleMahad@users.noreply.github.com>
2020-07-06 04:17:48 +0900

    1st

--- 2011.cpp
+++ 2011.cpp
@@ -4,21 +4,25 @@
 #include <limits.h>
 #include <cstring>
 #include <vector>
+#include <string>
+
 using namespace std;
-int N;
-int K;
-char
-int S[27];
+int S[100];
 int DP[5001];
 int main(){
-    cin >> N;
+    string str;
+    cin >> str;
     memset(DP,0,sizeof(DP));
+    memset(S,0,sizeof(S));
     S[0]=0;
     for(int i=1;i<=26;i++){
         S[i]=1;
     }
-    for(int i=0;i<=N;i++){
-        DP[i]=(DP[i]+DP[i/10]*S[i%10]+DP[i/100]*S[i%100])%1000000;
+    DP[0] = S[(int)str.at(0)-48];
+    if(str.size()>1)
+        DP[1] = S[((int)str.at(0)-48)*10+((int)str.at(1)-48)]*S[((int)str.at(0)-48)]+S[(int)str.at(0)-48]*S[(int)str.at(1)-48];
+    for(int i=2;i<str.size();i++){
+            DP[i]=(DP[i-1]*S[(int)str.at(i)-48] + DP[i-2]*S[((int)str.at(i-1)-48)*10+((int)str.at(i)-48)]*S[((int)str.at(i-1)-48)])%1000000;
     }
-    cout << DP[N] << endl;
+    cout << DP[str.size()-1] << endl;
 }
