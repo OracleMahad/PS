0311cf90eef2e48df597590ce377589c0c246396
OracleMahad <61798131+OracleMahad@users.noreply.github.com>
2020-07-14 23:59:46 +0900

    1st

---
+++ 10799.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstdlib>
+#include <algorithm>
+#include <limits.h>
+#include <cstring>
+#include <vector>
+#include <string>
+using namespace std;
+int N;
+string order;
+vector<int> v;
+int main(){
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    cin >> N;
+ 
+
+    }
+}
--- 9012.cpp
+++ 9012.cpp
@@ -15,7 +15,22 @@ int main(){
     cin >> N;
     for(int i=0;i<N;i++){
         cin >> order;
-        
-    }
+        int stacksize=0;
+        for(int j=0;j<order.size();j++){
+            if(order.at(j) == '(' ){
+                stacksize++;
+            }
+            else{
+                stacksize--;
+            }
+            if(stacksize<0)break;
+        }
+        if(stacksize!=0){
+            cout << "NO" << "\n";
+        }
+        else{
+            cout << "YES"<< "\n";
+        }
 
+    }
 }
