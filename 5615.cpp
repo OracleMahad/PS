6e1b1ba76d72318487617895f35c87b2e817f812
OracleMahad <61798131+OracleMahad@users.noreply.github.com>
2020-08-16 22:23:59 +0900

    commit

--- 5615.cpp
+++ 5615.cpp
@@ -1,25 +1,47 @@
 #include <iostream>
+#include <cstdlib>
+#include <algorithm>
+#include <cstring>
+#include <list>
+#include <vector>
+#include <queue>
+#include <cmath>
 using namespace std;
-#define MAXV  4294967296
-//long long int minfact[MAXV];
-int main() {
-    bool * minfact = new bool[MAXV];
-	for (unsigned long int i = 2; i < MAXV ; i++) {
-		minfact[i] = true;
-	}
-	for (unsigned long int i = 2; i*i < MAXV; i++) {
-		if (minfact[i] == true) {
-			for (long long int j = i*i; j < MAXV; j+=i) {
-                minfact[j] = false;
-			}
-		}
-	}
-    cout << minfact[MAXV-10];
-	/*int T;
-	cin >> T;
-	int k;
-	for (int i = 0; i < T; i++) {
-		cin >> k;
+int T, N, M, V;
+vector <int> graph[100001];
+int visitD[1001];
 
-	}*/
+//0: untracked, 2 : cycle
+int DFS_cycle(int start){
+    cout << start << " ";
+    visitD[start]=1;
+	vector <int>::iterator iter;
+	
+    for(int i =1;i<=N;i++){
+        
+		
+    }
+}
+
+int main(){
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+	cin >> T;
+	for(int i=0;i<T;i++){
+		memset(visitD,0,sizeof(visitD));
+		for(int j=1;j<=100001;j++){
+			graph[i].clear();
+		}
+		cin >> N;
+		for(int i=1;i<=N;i++){
+			int input;
+			cin >> input ;
+			graph[i].push_back(input);
+		}
+		for(int i=1;i<=N;i++){
+			DFS_cycle(V);
+		}
+
+		cout <<"\n";
+	}
 }
