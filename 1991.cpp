333fb0668da1cb55592bdb43c358df7eee92223b
OracleMahad <61798131+OracleMahad@users.noreply.github.com>
2020-08-28 22:16:24 +0900

    commit

---
+++ 1991.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <cstdlib>
+#include <algorithm>
+#include <cstring>
+#include <vector>
+#include <climits>
+
+using namespace std;
+int N;
+struct node
+{
+    char par;
+    struct  node *left=NULL;
+    struct  node *right=NULL;
+
+};
+
+
+int main(){
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    struct node nd[27];
+    cin >> N; 
+    for(int i=0;i<N;i++){
+        char a, b, c;
+        cin a >> b >> c;
+        
+    }
+    
+    
+}
--- 2146.cpp
+++ 2146.cpp
@@ -3,65 +3,70 @@
 #include <algorithm>
 #include <cstring>
 #include <vector>
-#include <string>
-#include <queue>
 #include <climits>
 
 using namespace std;
 int N;
 int island[102][102];
 int cell[102][102];
+int len=INT_MAX;
+vector <pair <int, int>> V;
 
-queue <pair <int, int>> Q;
-
-void BFS(){
-    while(!Q.empty()){
-        int a, b;
-        a=Q.front().first;
-        b=Q.front().second;
-        Q.pop();
-
-        if(island[a+1][b]!='1'){
-            island[a+1][b]='2';
-            cell[a+1][b]=cell[a][b]+1;
-            Q.push(make_pair(a+1,b));
+void DFS(int a, int b, int num){
+        island[a][b]=num;
+        V.push_back(make_pair(a,b));
+        if(island[a+1][b]==1){
+            island[a+1][b]=num;
+            DFS(a+1,b,num);
         }
-        if(island[a-1][b]=='1'){
-            island[a-1][b]='2';
-            cell[a-1][b]=cell[a][b]+1;
-            Q.push(make_pair(a-1,b));
+        if(island[a-1][b]==1){
+            island[a-1][b]=num;
+            DFS(a-1,b,num);
         }
-        if(island[a][b+1]=='1'){
-            island[a][b+1]='2';
-            cell[a][b+1]=cell[a][b]+1;
-            Q.push(make_pair(a,b+1));
+        if(island[a][b+1]==1){
+            island[a][b+1]=num;
+            DFS(a,b+1,num);
         }
-        if(island[a][b-1]=='1'){
-            island[a][b-1]='2';
-            cell[a][b-1]=cell[a][b]+1;
-            Q.push(make_pair(a,b-1));
+        if(island[a][b-1]==1){
+            island[a][b-1]=num;
+            DFS(a,b-1,num);
         }
-    }
 }
-
 int main(){
-    
     cin.tie(NULL);
     ios_base::sync_with_stdio(false);
 
     cin >> N; 
 
     memset(cell, 0, sizeof(cell));
-    memset(island, 0, sizeof(island));
+    memset(island, -1, sizeof(island));
 
     for(int i=1;i<=N;i++){
         for(int j=1;j<=N;j++){
             cin >> island[i][j];
         }
     }
-    DFS();
-    BFS();
 
-    cout << cell[N][M] << "\n";
+    int num=1;    
+    for(int i=1;i<=N;i++){
+        for(int j=1;j<=N;j++){
+            if(island[i][j]==1){
+                num++;
+                DFS(i,j,num);
+            }
+        }
+    }
+    vector <pair <int, int>>::iterator iter1;
+    vector <pair <int, int>>::iterator iter2;
+    for(iter1=V.begin();iter1!=V.end();iter1++){
+        for(iter2=V.begin();iter2!=iter1;iter2++){
+            if(island[iter1->first][iter1->second]!=island[iter2->first][iter2->second]){
+                len=min(len,abs(iter1->first-iter2->first)+abs(iter1->second-iter2->second)-1);
+            }
+        }
+    }
+    
+
+    cout << len << "\n";
     
 }
