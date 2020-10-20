1993cea8179fe3f681f376c9dd3f404a94a7e24c
OracleMahad <61798131+OracleMahad@users.noreply.github.com>
2020-10-20 23:51:52 +0900

    commit

---
+++ 11729.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cstdlib>
+#include <algorithm>
+#include <cstring>
+#include <vector>
+#include <climits>
+
+using namespace std;
+int N, K;
+
+void hanoi(int from, int to, int n){
+
+}
+
+int main(){
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    cin >> N;
+
+    hanoi( 1, 3, N);
+
+    cout << numm1 << "\n" << num0 << "\n" << num1 <<"\n";
+}
