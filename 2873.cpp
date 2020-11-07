34bd85d2a1cc1154005689ba0ae89d40a6203f25
OracleMahad <61798131+OracleMahad@users.noreply.github.com>
2020-11-07 20:09:51 +0900

    1st commit

--- 2873.cpp
+++ 2873.cpp
@@ -17,7 +17,7 @@ int main(){
     for(int i=0;i<R;i++){
         for(int j=0;j<C;j++){
             cin >> mat[i][j];
-            if(i%2==1 && j%2==1)
+            if(i%2==1 ^ j%2==1)
                 if(mat[i][j]<min_black){
                     min_black= mat[i][j];
                     x=i;
@@ -33,7 +33,7 @@ int main(){
             }
             if(i<R-1)cout << "D";
         }    
-    } else if(C%1==1){
+    } else if(C%2==1){
         for(int i=0;i<C;i++){
             for(int j=0;j<R-1;j++){
                 cout  << ((i%2==0) ? "D": "U");
@@ -41,13 +41,29 @@ int main(){
             if(i<C-1)cout << "R";
         }  
     } else {
+        int way =0;
         for(int i=0;i<R;i++){
-            if(i==x||i==x+1){
-                
-                j+=2;
-            } else {
+            if(i==x||(i==x-1) && i%2==0){
+                int bf = (y/2);
+                int af = C/2 - bf - 1;
+                while(bf--){
+                    cout <<"DRUR";
+                }
+                if(i==x)
+                    cout <<"DR";
+                else
+                    cout <<"RD";
+                while(af--){
+                    cout <<"RURD";
+                }
+
+                i++;
+                way=1;
+                if(i<R-1)cout << "D";
+            } 
+            else {
                 for(int j=0;j<C-1;j++){
-                    cout  << ((i%2==0) ? "R": "L");
+                    cout  << ((i%2==way) ? "R": "L");
                 }
                 if(i<R-1)cout << "D";
             }
